64ebc0b1f9f5b1f928710b6ea8ed84176855cf95
Javi Pernas <javier.pernas2@udc.es>
2025-04-14 11:59:33 +0200

    Firebase Project Link

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,9 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, indexedDBLocalPersistence } from "firebase/auth";
 import { firebaseConfig } from "./firebaseConfig";
+// import { getAnalytics } from "firebase/analytics";
+
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+// const analytics = getAnalytics(app);
 
 auth.setPersistence(indexedDBLocalPersistence);
